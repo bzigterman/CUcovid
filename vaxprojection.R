bca8c97492908d18b4cf5bcc5038d8307809ab82
Ben Zigterman <ben@bzigterman.com>
2021-02-03 20:44:59 -0600

    add second dose data, filter for first dose chart

--- vaxprojection.R
+++ vaxprojection.R
@@ -7,9 +7,18 @@ library(scales)
 vax_proj <- read_sheet("1HbhKu6Uby8b6YKGIFhTYQRiRX5Y2T8aS2DBqG7cz_eE",
                   sheet = 2)
 write.csv(vax_proj,"data/vax_proj.csv", row.names = FALSE)
+vax_proj_first_dose <- vax_proj %>%
+  select(date,
+         `Percent Receiving First Dose`,
+         first_dose,
+         new_dose1,
+         avg_daily_first_dose,
+         pct_dose1) %>%
+  filter(first_dose != is.null(first_dose))
 
-# chart
-ggplot(vax_proj, aes(x = as.Date(date), y = pct, 
+
+# chart of first dose
+ggplot(vax_proj_first_dose, aes(x = as.Date(date), y = pct_dose1, 
                      colour = `Percent Receiving First Dose`,
                      linetype = `Percent Receiving First Dose`)) +
   geom_line(size = 1.5) +
@@ -36,3 +45,29 @@ ggsave("VaccineProjection.png",
         path = "../bzigterman.github.io/images/",
         width = 8, height = 32/7, dpi = 150)
  
+
+# chart of both doses
+ggplot(vax_proj, aes(x = as.Date(date), y = pct_dose1, 
+                     colour = `Percent Receiving First Dose`,
+                     linetype = `Percent Receiving First Dose`)) +
+  geom_line(size = 1.5) +
+  geom_line(aes(x = as.Date(date), y = pct_dose2,
+                colour = `Percent Receiving First Dose`,
+                linetype = `Percent Receiving First Dose`)) +
+  ggtitle("When a Percent of Champaign County Residents Might Receive First Vaccine Dose",
+          "Based on average new first doses administered over the past week")+
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_y_continuous(labels = percent, 
+                     position = "right",
+                     expand = expansion(mult = c(0,0))) +
+  scale_x_date(expand = c(0,0)) +
+  scale_colour_manual(values = c("#800080", "#008040")) +
+  theme(text = element_text(family = "Barlow"),
+        axis.text.y = element_text(size = 15),
+        axis.text.x = element_text(size = 15),
+        plot.title = element_text(size = 18, family = "Oswald"),
+        legend.position = c(.15,.5),
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        legend.text = element_text(size = 12)) 
