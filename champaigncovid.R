4c02aabb24d68eed7ae3c8aaa7d6ef50a154bc3e
Ben Zigterman <ben@bzigterman.com>
2021-02-15 12:20:32 -0600

    Delete champaigncovid.R
    using CUcovid.R instead

--- champaigncovid.R
+++
@@ -1,9 +0,0 @@
-library(googlesheets4)
-library(dplyr)
-library(ggplot2)
-library(scales)
-library(zoo)
-
-# get the data from google sheet
-regioncovid <- read_sheet("1UUGDwV5qahPos-bhWUfzf4Y1WYXEh-I0JBOJaoGMrJs",
-                          sheet = 1)
