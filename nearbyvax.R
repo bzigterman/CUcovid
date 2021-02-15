1552fcbd3c7060c79bf175630f359c5fd8486aab
Ben Zigterman <ben@bzigterman.com>
2021-02-15 16:12:59 -0600

    Create nearbyvax.R

---
+++ nearbyvax.R
@@ -0,0 +1,72 @@
+library(rio)
+library(tidyverse)
+library(lubridate)
+library(googlesheets4)
+library(dplyr)
+library(ggplot2)
+library(scales)
+library(zoo)
+library(clipr)
+
+# import and clean data
+vax_nearby <- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Champaign",
+                                  format = "csv") %>%
+  rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Vermilion",
+              format = "csv") %>%
+  rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Ford",
+              format = "csv") %>%
+  rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Piatt",
+              format = "csv") 
+
+
+vax_champaign <- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Champaign",
+            format = "csv") 
+vax_vermilion <- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Vermilion",
+                             format = "csv") 
+vax_ford <- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Ford",
+                             format = "csv") 
+vax_edgar<- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Edgar",
+                        format = "csv") 
+vax_douglas<- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Douglas",
+                          format = "csv") 
+vax_piatt<- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Piatt",
+                        format = "csv") 
+vax_iroquois <- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Iroquois",
+                            format = "csv") 
+vax_dewitt<- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=De%20Witt",
+                         format = "csv") 
+vax_macon<- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Macon",
+                        format = "csv") 
+vax_moultrie<- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Moultrie",
+                           format = "csv") 
+vax_nearby <- full_join(vax_champaign, vax_vermilion) %>%
+  full_join(vax_ford) %>%
+  full_join(vax_edgar) %>%
+  full_join(vax_douglas) %>%
+  full_join(vax_piatt) %>%
+  full_join(vax_iroquois) %>%
+  full_join(vax_dewitt) %>%
+  full_join(vax_macon) %>%
+  full_join(vax_moultrie) %>%
+  mutate(Date = mdy_hms(Report_Date)) %>%
+  mutate(PersonsDose1 = AdministeredCount - PersonsFullyVaccinated) #%>%
+  #mutate(PercentDose1 = PersonsDose1/209983) #%>%
+ # mutate(Dose1Change = PersonsDose1 - lag(PersonsDose1)) %>%
+#  mutate(Dose2Change = PersonsFullyVaccinated - lag(PersonsFullyVaccinated))
+
+# line chart comparing all the counties
+ggplot(vax_nearby, aes(x = as.Date(Date), y = PctVaccinatedPopulation,
+                       colour = CountyName)) +
+  geom_line() +
+  scale_y_continuous(labels = percent, 
+                     position = "right",
+                     expand = expansion(mult = c(0,.05))) +
+  scale_x_date(expand = expansion(mult = c(0,0))) +
+  guides(fill = FALSE) +
+  xlab(NULL) +
+  ylab(NULL) +
+  ggtitle("Percent of Population Fully Vaccinated in Nearby Counties")+
+  theme(text = element_text(family = "Barlow"),
+        axis.text.y = element_text(size = 13),
+        axis.text.x = element_text(size = 13),
+        plot.title = element_text(size = 22, family = "Oswald")) 
