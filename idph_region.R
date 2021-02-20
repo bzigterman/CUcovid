7c20e9cb2576e1afd2135d7b9faebbe5b3045fc7
Ben Zigterman <ben@bzigterman.com>
2021-02-19 21:14:32 -0600

    new region cases chart

--- idph_region.R
+++ idph_region.R
@@ -11,7 +11,9 @@ library(clipr)
 idph_region6 <- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetResurgenceData?format=csv&regionID=6&daysIncluded=0",
                             format = "csv") %>%
   filter(RegionID == 6) %>%
-  mutate(Date = mdy_hms(ReportDate)) 
+  mutate(Date = mdy_hms(ReportDate)) %>%
+  mutate(avgnewcases = rollmean(PositiveTests, k = 7, 
+                                       fill = NA, align = "right"))
 write.csv(idph_region6,"idph/region6.csv", row.names = FALSE)
 
 idph_region6noUI <- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetResurgenceData?format=csv&regionID=6&daysIncluded=0",
@@ -59,7 +61,6 @@ ggsave("region6hospitalbeds.png",
 
 
 # region 6 new hospitalizations in use chart
-
 ggplot(idph_region6, aes(x = as.Date(Date), y = CLIAdmissions_RollingAvg)) +
   geom_col(fill = "#660101",
            alpha = .25) +
@@ -81,6 +82,32 @@ ggplot(idph_region6, aes(x = as.Date(Date), y = CLIAdmissions_RollingAvg)) +
 ggsave("region/region6newhospital.png", 
        width = 8, height = 32/7, dpi = 320)
 ggsave("region6newhospital.png", 
+       path = "../bzigterman.github.io/images/",
+       width = 8, height = 32/7, dpi = 320)
+
+# new cases chart for region 6
+ggplot(idph_region6, aes(x = as.Date(Date), y = PositiveTests)) +
+  geom_col(fill = "#B45F06",
+           alpha = .25) +
+  geom_line(aes(y = avgnewcases),
+            colour = "#B45F06",
+            size = 1.5) +
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_y_continuous(labels = comma, 
+                     position = "right",
+                     expand = expansion(mult = c(0,.05))) +
+  scale_x_date(expand = c(0,0)) +
+  guides(fill = FALSE) +
+  ggtitle("New Cases in Region 6",
+          "With seven-day moving average. Source: IDPH")+
+  theme(text = element_text(family = "Barlow"),
+        axis.text.y = element_text(size = 13),
+        axis.text.x = element_text(size = 13),
+        plot.title = element_text(size = 22, family = "Oswald")) 
+ggsave("region/region6newcases.png", 
+       width = 8, height = 32/7, dpi = 320)
+ggsave("region6newcases.png", 
        path = "../bzigterman.github.io/images/",
        width = 8, height = 32/7, dpi = 320)
 
