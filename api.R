a4790e4a0bcecd6488481f0435360a6a8b5c8588
Ben Zigterman <ben@bzigterman.com>
2021-02-24 22:33:21 -0600

    Create api.R

---
+++ api.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(httr)
+library(jsonlite)
+
+url <- "https://idph.illinois.gov/DPHPublicInformation/"
+
+GET("https://idph.illinois.gov/DPHPublicInformation/api/COVID/GetCountyHistorical?countyName=Champaign")
+GET("https://idph.illinois.gov/DPHPublicInformation/api/COVID/GetCountyHistorical")
+rates <- GET("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetCountyRates")
+content(rates)
+rates <- fromJSON(content(rates, "text"))
+rates <- fromJSON(content(rates, "text"))
+rates
+path <- "api/COVIDExport/GetCountyRates"
+
+GET(url, path = "api/COVID/GetHospitalizationResults")
+GET("https://idph.illinois.gov/DPHPublicInformation/api/COVID/GetHospitalizationResults")
+hospitalization <- GET("https://idph.illinois.gov/DPHPublicInformation/api/COVID/GetHospitalizationResults")
+content(hospitalization)
+hospitalization <- fromJSON(content(hospitalization, "text"))
+regionhosp <- hospitalization$regionValues
+
+?fromJSON
+?GET
