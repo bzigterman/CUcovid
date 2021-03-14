86c0212a3934155f99996ca0a64485537ac728a4
Ben Zigterman <ben@bzigterman.com>
2021-03-13 20:34:19 -0600

    more experimenting with boxr

--- uibox.R
+++ uibox.R
@@ -8,7 +8,7 @@ box_auth()
 box_dl(file_id = "780293011768",
        file_name = "illinois_shield_covid_data-2021-02-25.csv")
 ?box_read_csv
-box_read_csv(file_id = "780293011768")
+box_read_csv(file_id = "781566755894")
 box_dl(file_id = "781566755894")
 ?box_ls
 box_ls(dir_id = "0")
@@ -18,3 +18,7 @@ box_collab_get(dir_id = "nrrnx2fn6sqt4hhuh2ckq2u6z5o4zmqg",
                file_id = "781566755894")
 box_dl(file_id = "781566755894")
 box_dl(file_id = "781566755894")
+box_collab_get(file_id = "781566755894")
+GET("https://api.box.com/2.0/shared_items?shared_link=https://uofi.app.box.com/s/nrrnx2fn6sqt4hhuh2ckq2u6z5o4zmqg/file/781566755894")
+GET("https://api.box.com/2.0/shared_items", add_headers(BoxApi = "https://uofi.app.box.com/s/nrrnx2fn6sqt4hhuh2ckq2u6z5o4zmqg/file/781566755894"))
+box_auth()
