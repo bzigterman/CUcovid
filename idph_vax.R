4bdefd9fdb292b1ce90a689154b9d5a243ffe863
Ben Zigterman <ben@bzigterman.com>
2021-03-21 14:30:48 -0500

    remove background

--- idph_vax.R
+++ idph_vax.R
@@ -65,12 +65,13 @@ ggplot(pivoted_vax_champaign,
   labs(title = "New Vaccine Doses Administered in Champaign County",
        subtitle =  "With seven-day moving average", 
        caption = "Source: Illinois Department of Public Health")+
-  theme_minimal() +
+  #theme_minimal() +
   theme(text = element_text(family = "Barlow"),
         axis.text.y = element_text(size = 13),
         axis.text.x = element_text(size = 13),
         plot.title = element_text(size = 22, family = "Oswald"),
         plot.caption = element_text(colour = "grey40"),
+        panel.background = element_blank(),
         legend.position = c(.15,.75),
         legend.background = element_blank(),
         legend.key = element_blank(),
@@ -145,13 +146,13 @@ xlab(NULL) +
   labs(title = "Percent of Champaign County Vaccinated",
        # subtitle = "With percent of total population.",
        caption = "Source: Illinois Department of Public Health") +
-  theme_minimal() +
+  #theme_minimal() +
   theme(text = element_text(family = "Barlow"),
         axis.text.y = element_text(size = 13),
         axis.text.x = element_text(size = 13),
         panel.grid = element_blank(),
         #panel.grid.minor.y = element_blank(),
-        #  panel.background = element_blank(),
+        panel.background = element_blank(),
         plot.caption = element_text(colour = "grey40"),
         plot.title = element_text(size = 22, family = "Oswald")) 
 
