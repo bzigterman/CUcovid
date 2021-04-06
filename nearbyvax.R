caee1a998bf177145f90d64215bca8beb555a9a1
Ben Zigterman <ben@bzigterman.com>
2021-04-06 17:52:13 -0500

    make more transparent

--- nearbyvax.R
+++ nearbyvax.R
@@ -599,7 +599,7 @@ ggplot(data = vax_nearby_facet,
            y = Percent,
            fill = Doses,
            colour = Doses)) +
-  geom_area(alpha = .7) +
+  geom_area(alpha = .6) +
   scale_fill_manual(labels = c("At least one dose",
                                  "Fully vaccinated"),
                     values = c("#d8cee8","#674EA7"),
