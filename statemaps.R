a00d6ff1828ac4013cf3ae288d54d0a095d3da3b
Ben Zigterman <ben@bzigterman.com>
2021-04-12 16:48:18 -0500

    update urls to csvs

--- statemaps.R
+++ statemaps.R
@@ -1,2 +1,3 @@
-https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetZip
-https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetCountyTestResults
+zipurl <- "https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetZip?format=csv"
+countysnapshoturl <- "https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetCountyTestResults?format=csv"
+hospitalizationurl <- "https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetHospitalUtilizationResults?format=csv"
