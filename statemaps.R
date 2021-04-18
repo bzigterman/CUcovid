11bf2eb5a4435cc50e584866223576bfe9e54eea
Ben Zigterman <ben@bzigterman.com>
2021-04-17 20:46:41 -0500

    add total deaths map

--- statemaps.R
+++ statemaps.R
@@ -16,15 +16,15 @@ state_rates_csv <- rio::import(ratesurl,
 state_rates_merged <- merge(il_counties, state_rates_csv,
                             by = "GEOID")
 
-# plot ----
-state_case_rate_map <- ggplot(data = state_rates_merged) + 
+# total case rate plot ----
+state_total_case_rate_map <- ggplot(data = state_rates_merged) + 
   geom_sf(data = state_rates_merged,
           mapping = aes(fill = confirmed_rate_per_100k),
           # color = "grey",
           size = .25) +
   scale_fill_gradient(low = "#F7EDE3",
                       high = "#B45F06",
-                      # labels = percent,
+                      labels = comma
                       # guide = guide_legend(title = NULL)
   ) +
   # geom_sf(data = nearby_cities_sf, size = .5) +
@@ -37,7 +37,7 @@ state_case_rate_map <- ggplot(data = state_rates_merged) +
   #           size = 2.9,
   #           col = "black",
   #           family = "Barlow") +
-  labs(title = "New Cases per 100,000 Residents",
+  labs(title = "Total Cases per 100,000 Residents",
        caption =  "Source: Illinois Department of Public Health",
        fill = NULL)+
   #theme_minimal() +
@@ -57,5 +57,36 @@ state_case_rate_map <- ggplot(data = state_rates_merged) +
         #legend.text = element_text(size = 13),
         plot.caption = element_text(colour = "grey40"),
         plot.title = element_text(size = 16, family = "Oswald")) 
-state_case_rate_map
+state_total_case_rate_map
+
+# total case rate plot ----
+state_total_death_rate_map <- ggplot(data = state_rates_merged) + 
+  geom_sf(data = state_rates_merged,
+          mapping = aes(fill = deaths_rate_per_10k),
+          # color = "grey",
+          size = .25) +
+  scale_fill_gradient(low = "#FADBDB",
+                      high = "#d90000",
+                      labels = comma) +
+  labs(title = "Total Deaths per 100,000 Residents",
+       caption =  "Source: Illinois Department of Public Health",
+       fill = NULL)+
+  #theme_minimal() +
+  theme(text = element_text(family = "Barlow"),
+        axis.text = element_blank(),
+        axis.line.x = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        #panel.grid.major.x = element_line(colour = "grey93"),
+        #legend.position = "none",
+        panel.grid.major = element_blank(),  
+        legend.position = c(.1,.9),
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        legend.key.size = unit(.5, "cm"),
+        panel.background = element_blank(),
+        #legend.text = element_text(size = 13),
+        plot.caption = element_text(colour = "grey40"),
+        plot.title = element_text(size = 16, family = "Oswald")) 
+state_total_death_rate_map
 
