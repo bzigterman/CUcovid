389c87dd676f6a05cff046d1d401a11f492a06f5
Ben Zigterman <ben@bzigterman.com>
2021-04-18 19:48:23 -0500

    add chicago to state maps

--- statemaps.R
+++ statemaps.R
@@ -1,3 +1,58 @@
+
+# getshapefiles ready ----
+
+# chicago_outline <- st_read(dsn = "Chicago",package = "sf")
+# ?st_read
+# ?st_drivers
+# #chicago_outline <- system.file(dsn = "Chicago",package = "sf")
+# chicago_outline <- st_read(system.file("Chicago",package = "sf"),
+#                            sf_column_name = thegeom)
+chicago_outline <- read_sf(dsn = "Chicago")
+
+#?read_sf
+# chicago_outline <-rio::import("https://data.cityofchicago.org/resource/qqq8-j68g.json",
+#             format = "json") 
+# 
+
+il_counties_clean <- il_counties %>%
+  mutate(variable = NULL) %>%
+  mutate(estimate = NULL) %>%
+  mutate(moe = NULL)
+
+
+chicago_outline <- chicago_outline %>%
+  transmute(NAME = name) %>%
+  mutate(GEOID = "606060606") %>%
+  mutate(objectid = NULL) %>%
+  mutate(shape_area = NULL) %>%
+  mutate(shape_len = NULL) %>%
+  st_transform("NAD83")
+
+cook_county <- il_counties_clean %>%
+  filter(GEOID == 17031) 
+
+cook_county_no_chicago <- st_difference(cook_county, chicago_outline) %>%
+  mutate(NAME.1 = NULL) %>%
+  mutate(GEOID.1 = NULL)
+
+cook_with_chicago <- rbind(cook_county_no_chicago,chicago_outline)
+
+il_counties_no_cook <- il_counties_clean %>%
+  filter(GEOID != 17031) 
+
+# 
+il_counties_with_chicago <- rbind(cook_with_chicago, il_counties_no_cook)
+#plot(il_counties_with_chicago)
+#?st_combine
+# ?full_join
+# ?st_crs
+# st_crs(il_counties)
+# st_crs(chicago_outline)
+# plot(il_counties_chicago)
+# ?st_join
+
+# get data ----
+
 zipurl <- "https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetZip?format=csv"
 countysnapshoturl <- "https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetCountyTestResults?format=csv"
 hospitalizationurl <- "https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetHospitalUtilizationResults?format=csv"
@@ -7,9 +62,9 @@ countysnapshotvaxurl <- "https://idph.illinois.gov/DPHPublicInformation/api/COVI
 #                   variables = "B19013_001", geometry = TRUE)
 
 state_cases_map_csv <- rio::import(countysnapshoturl,
-                             format = "csv") 
+                                   format = "csv") 
 state_vax_map_csv <- rio::import(countysnapshotvaxurl,
-                                   format = "json") 
+                                 format = "json") 
 state_vax_map_csv <- state_vax_map_csv$VaccineAdministration
 
 ratesurl <- "https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetCountyRates?format=csv"
@@ -18,8 +73,16 @@ state_rates_csv <- rio::import(ratesurl,
                                format = "csv") %>%
   filter(County != "Illinois") %>%
   filter(County != "Chicago") %>%
-  mutate(GEOID = fips("IL", county = County))
-state_rates_merged <- merge(il_counties, state_rates_csv,
+  mutate(GEOID = fips("IL", county = County)) 
+
+state_rates_with_chicago <- rio::import(ratesurl,
+                                        format = "csv") %>%
+  filter(County == "Chicago") %>%
+  mutate(GEOID = "606060606") %>%
+  full_join(state_rates_csv)
+
+state_rates_merged <- merge(il_counties_with_chicago, 
+                            state_rates_with_chicago,
                             by = "GEOID")
 
 state_vax_clean <- state_vax_map_csv %>%
@@ -28,25 +91,21 @@ state_vax_clean <- state_vax_map_csv %>%
   filter(CountyName != "Illinois") %>%
   filter(CountyName != "Out Of State") %>%
   mutate(GEOID = fips("IL", county = CountyName))
-state_vax_merged <- merge(il_counties, state_vax_clean,
+
+state_vax_with_chicago <- state_vax_map_csv %>%
+  filter(CountyName == "Chicago") %>%
+  mutate(GEOID = "606060606") %>%
+  full_join(state_vax_clean)
+
+
+state_vax_merged <- merge(il_counties_with_chicago, 
+                          state_vax_with_chicago,
                           by = "GEOID")
 
 il_zip_cases <- rio::import(zipurl,
                             format = "csv") 
 
-# chicago_outline <- st_read(dsn = "Chicago",package = "sf")
-# chicago_outline <- system.file(dsn = "Chicago",package = "sf")
-# chicago_outline <- read_sf(dsn = "Chicago/geo_export_fed7fb1c-918b-490b-9aa3-4d19f22a3ca9.shp",package = "sf")
-# ?read_sf
-# chicago_outline <-rio::import("https://data.cityofchicago.org/resource/qqq8-j68g.json",
-#             format = "json") 
-# 
-# chicago_outline <- chicago_outline %>%
-#   mutate(NAME = name) %>%
-#   mutate(geometry = the_geom)
-# 
-# il_counties_chicago <- full_join(chicago_outline, il_counties)
-# ?full_join
+
 # total case rate plot ----
 state_total_case_rate_map <- ggplot(data = state_rates_merged) + 
   geom_sf(data = state_rates_merged,
@@ -80,7 +139,7 @@ state_total_case_rate_map <- ggplot(data = state_rates_merged) +
         #panel.grid.major.x = element_line(colour = "grey93"),
         #legend.position = "none",
         panel.grid.major = element_blank(),  
-        legend.position = c(.1,.9),
+        #legend.position = c(.1,.9),
         legend.background = element_blank(),
         legend.key = element_blank(),
         legend.key.size = unit(.5, "cm"),
@@ -90,7 +149,7 @@ state_total_case_rate_map <- ggplot(data = state_rates_merged) +
         plot.title = element_text(size = 16, family = "Oswald")) 
 state_total_case_rate_map
 
-# total case rate plot ----
+# total death rate plot ----
 state_total_death_rate_map <- ggplot(data = state_rates_merged) + 
   geom_sf(data = state_rates_merged,
           mapping = aes(fill = deaths_rate_per_10k),
@@ -100,6 +159,7 @@ state_total_death_rate_map <- ggplot(data = state_rates_merged) +
                       high = "#d90000",
                       labels = comma) +
   labs(title = "Total Deaths per 100,000 Residents",
+      # subtitle="Per 100,000 residents",
        caption =  "Source: Illinois Department of Public Health",
        fill = NULL)+
   #theme_minimal() +
@@ -109,9 +169,9 @@ state_total_death_rate_map <- ggplot(data = state_rates_merged) +
         axis.ticks = element_blank(),
         axis.title = element_blank(),
         #panel.grid.major.x = element_line(colour = "grey93"),
-        #legend.position = "none",
+        legend.position = "left",
         panel.grid.major = element_blank(),  
-        legend.position = c(.1,.9),
+        #legend.position = c(.1,.9),
         legend.background = element_blank(),
         legend.key = element_blank(),
         legend.key.size = unit(.5, "cm"),
@@ -140,9 +200,9 @@ state_fully_vax_map <- ggplot(data = state_vax_merged) +
         axis.ticks = element_blank(),
         axis.title = element_blank(),
         #panel.grid.major.x = element_line(colour = "grey93"),
-        #legend.position = "none",
+        legend.position = "right",
         panel.grid.major = element_blank(),  
-        legend.position = c(.1,.9),
+        #legend.position = c(.1,.9),
         legend.background = element_blank(),
         legend.key = element_blank(),
         legend.key.size = unit(.5, "cm"),
@@ -152,4 +212,17 @@ state_fully_vax_map <- ggplot(data = state_vax_merged) +
         plot.title = element_text(size = 16, family = "Oswald")) 
 state_fully_vax_map
 
-state_total_death_rate_map + state_fully_vax_map
+state_total_death_rate_map + 
+  labs(title = "Total Deaths per 100,000 Residents",
+      # subtitle="Per 100,000 residents",
+       caption =  "") +
+  theme(plot.title = element_text(size = 10, family = "Oswald"),
+        legend.position = "left") +
+  state_fully_vax_map +
+  theme(plot.title = element_text(size = 10, family = "Oswald"),
+        legend.position = "right") +
+  
+ggsave("map/IL_deaths_vax_map.png", width = 8, height = 8*(628/1200), dpi = 320)
+ggsave("IL_deaths_vax_map.png", 
+       path = "../bzigterman.github.io/images/",
+       width = 8, height = 8*(628/1200), dpi = 320)
