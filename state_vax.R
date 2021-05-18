ae24300cb57f2f7649b57cc622827238b0505108
Ben Zigterman <ben@bzigterman.com>
2021-05-18 15:01:36 -0500

    new facet chart of state deaths and cases

--- state_vax.R
+++ state_vax.R
@@ -200,3 +200,63 @@ ggsave("IL_new_deaths.png",
 
 # library(plotly)
 # ggplotly(state_deaths)
+# facet state new cases and deaths  ----
+idph_cases_deaths_IL <- idph_cases_IL %>%
+  select(Date, avg_new_cases, avg_new_deaths) %>%
+  mutate("New Cases" =  avg_new_cases) %>%
+  mutate("New Deaths" =  avg_new_deaths) %>%
+  pivot_longer(cols = c("New Cases","New Deaths"),
+               values_to = "Number",
+               names_to = "New")
+
+# mutate("New Cases" =  avgnewcases) %>%
+#   mutate("New Tests" =  avgnewtests) %>%
+#   pivot_longer(cols = c("New Tests","New Cases"),
+               
+
+ggplot(idph_cases_deaths_IL,
+       #cases_and_vax, 
+       aes(x = as.Date(Date),
+           y = Number,
+           colour = New)) +
+  geom_line() +
+  facet_wrap(~ New, scales = "free_y",
+             ncol = 1) +
+  labs(#title = "COVID-19 at the University of Illinois",
+    #subtitle = "With moving seven-day average",
+    caption = "Source: Illinois Department of Public Health.\nNote: Moving seven-day average.") +
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_x_date(expand = c(0,0)) +
+  scale_y_continuous(labels = label_comma(accuracy = 1),
+                     #   limits = c(0,60000),
+                     position = "right",
+                     expand = expansion(mult = c(0,.05))
+  ) +
+  expand_limits(y = 0) +
+  scale_colour_manual(guide = FALSE,
+                      values = c("#B45F06",
+                                 "#d90000","#674EA7","#674EA7")) +
+  #theme_minimal() +
+  #theme_classic() +
+  theme(text = element_text(family = "Barlow"),
+        axis.text.y = element_text(size = 10),
+        #  axis.line.x = element_line(colour = "black"),
+        #panel.background = element_rect(fill = "grey98"),
+        axis.text.x = element_text(size = 8),
+        #axis.line.x = element_line(colour = "grey"),
+        #axis.ticks = 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey93"),
+        #panel.grid = element_blank(),
+        #panel.border = element_rect(colour = "grey", fill = NA),
+        strip.text = element_text(size = 11),
+        strip.background = element_blank(),
+        plot.caption = element_text(colour = "grey40"),
+        plot.title = element_text(size = 18, family = "Oswald"))
+
+ggsave("region/state_Cases_Tests.png", width = 5, height = 8*(628/1200), dpi = 320)
+ggsave("state_Cases_Tests.png", 
+       path = "../bzigterman.github.io/images/",
+       width = 5, height = 8*(628/1200), dpi = 320)
