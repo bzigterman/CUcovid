101ef549b75d12511d44d5ba5be2e8cc13f9091e
Ben Zigterman <ben@bzigterman.com>
2021-05-22 21:01:44 -0500

    add idph cases and deaths chart

--- gh_action.R
+++ gh_action.R
@@ -376,7 +376,7 @@ cdc_total_vax <- ggplot(data = cdc_vaccines_geo_merged) +
         plot.title = element_text(size = 16, family = "Georgia")) 
 cdc_total_vax
 
-# combined vax maps ----
+## combined vax maps ----
 cdc_total_vax + 
   labs(title = "Percent Fully Vaccinated",
        subtitle = "Total Population",
@@ -404,3 +404,60 @@ cdc_total_vax +
 
 ggsave("gh_action/CDC_vax_combined.png", 
        width = 8, height = 8*(628/1200), dpi = 320)
+
+# idph data ----
+## case data ----
+
+idph_cases_IL <- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVID/GetCountyHistorical?countyName=Illinois",
+                             format = "json") 
+idph_cases_IL <- idph_cases_IL$values %>%
+  mutate(population = illinoispop)  %>%
+  mutate(new_cases = confirmed_cases - lag(confirmed_cases)) %>%
+  mutate(new_deaths = deaths - lag(deaths)) %>%
+  mutate(avg_new_cases = rollmean(new_cases, k = 7, 
+                                  fill = NA, align = "right")) %>%
+  mutate(avg_new_deaths = rollmean(new_deaths, k = 7, 
+                                   fill = NA, align = "right")) %>%
+  mutate(Date = ymd_hms(reportDate))
+
+idph_cases_deaths_IL <- idph_cases_IL %>%
+  select(Date, avg_new_cases, avg_new_deaths) %>%
+  mutate("New Cases" =  avg_new_cases) %>%
+  mutate("New Deaths" =  avg_new_deaths) %>%
+  pivot_longer(cols = c("New Cases","New Deaths"),
+               values_to = "Number",
+               names_to = "New")
+
+## facet map of IL cases and deaths ----
+ggplot(idph_cases_deaths_IL,
+       aes(x = as.Date(Date),
+           y = Number,
+           colour = New)) +
+  geom_line() +
+  facet_wrap(~ New, scales = "free_y",
+             ncol = 1) +
+  labs(caption = "Source: Illinois Department of Public Health.\nNote: Moving seven-day average.") +
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_x_date(expand = c(0,0)) +
+  scale_y_continuous(labels = label_comma(accuracy = 1),
+                     position = "right",
+                     expand = expansion(mult = c(0,.05))
+  ) +
+  expand_limits(y = 0) +
+  scale_colour_manual(guide = FALSE,
+                      values = c("#B45F06",
+                                 "#d90000","#674EA7","#674EA7")) +
+  theme(text = element_text(family = "Verdana"),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 8),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey93"),
+        strip.text = element_text(size = 11),
+        strip.background = element_blank(),
+        plot.caption = element_text(colour = "grey40"),
+        plot.title = element_text(size = 18, family = "Georgia"))
+
+ggsave("gh_action/state_Cases_Tests.png", width = 5, height = 8*(628/1200), dpi = 320)
+
