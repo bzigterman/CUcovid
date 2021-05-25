f13d61d51ede5520bc4a830294b69836cfb5d4c5
Ben Zigterman <ben@bzigterman.com>
2021-05-25 18:35:49 -0500

    add champaign facet

--- gh_action.R
+++ gh_action.R
@@ -522,3 +522,70 @@ ggplot(idph_region6_cases_hospital,
 
 ggsave("gh_action/region_Cases_Hospital.png", 
        width = 5, height = 8*(628/1200), dpi = 320)
+
+# idph champaign county cases ----
+champaignpop <- 209983
+
+idph_cases_champaign <- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVID/GetCountyHistorical?countyName=Champaign",
+                                    format = "json") 
+idph_cases_champaign <- idph_cases_champaign$values %>%
+  mutate(population = champaignpop)  %>%
+  mutate(new_cases = confirmed_cases - lag(confirmed_cases)) %>%
+  mutate(new_deaths = deaths - lag(deaths)) %>%
+  mutate(avg_new_cases = rollmean(new_cases, k = 7, 
+                                  fill = NA, align = "right")) %>%
+  mutate(monthlydead = rollmean(new_deaths, k = 31, 
+                                   fill = NA, align = "right")*31)  %>%
+  mutate(Date = ymd_hms(reportDate)) 
+
+idph_vax_champaign <- rio::import("https://idph.illinois.gov/DPHPublicInformation/api/COVIDExport/GetVaccineAdministration?format=csv&countyName=Champaign",
+                                  format = "csv") %>%
+  mutate(Date = mdy_hms(Report_Date)) 
+
+idph_cases_vax <- full_join(idph_cases_champaign, idph_vax_champaign) %>%
+  select(Date, PersonsFullyVaccinated, AdministeredCountRollAvg,
+         monthlydead, avg_new_cases)
+
+idph_cases_vax_longer <- idph_cases_vax %>%
+  pivot_longer(!Date,
+               values_to = "values",
+               names_to = "names") %>%
+  mutate(names = recode(names, 
+                           "PersonsFullyVaccinated" = "3. People Fully Vaccinated",
+                           "avg_new_cases" = "1. Average New Cases",
+                           "monthlydead" = "2. Deaths in Past Month",
+                           "AdministeredCountRollAvg" = "4. Average New Vaccine Doses"))  %>%
+  mutate(short_date = paste(month(Date, label = TRUE, abbr = FALSE),
+                            mday(Date)))
+
+
+## plot ----
+ggplot(filter(idph_cases_vax_longer, Date > as.Date("2020-12-15")),
+       aes(x = as.Date(Date),
+           y = values,
+           colour = names)) +
+  geom_line() +
+  facet_wrap(~ names, scales = "free_y") +
+  labs(title = "COVID-19 Metrics Since Vaccinations Began Dec. 16",
+       caption = paste("Source: IDPH. Data updated",
+                       tail(idph_cases_vax_longer$short_date,1))) +
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_x_date(expand = c(0,0)) +
+  scale_y_continuous(labels = label_comma(accuracy = 1),
+                     position = "right",
+                     expand = expansion(mult = c(0,.05))
+  ) +
+  expand_limits(y = 0) +
+  scale_colour_manual(guide = FALSE,
+                      values = c("#B45F06","#d90000","#674EA7","#674EA7")) +
+  theme(axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 8),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey93"),
+        strip.text = element_text(size = 11),
+        strip.background = element_blank(),
+        plot.caption = element_text(colour = "grey40"))
+ggsave("gh_action/Champaign_facet.png", 
+       width = 6, height = 8*(628/1200), dpi = 320)
