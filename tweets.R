668b19834620f3d9971e2c5a99c421ebbba97566
Ben Zigterman <ben@bzigterman.com>
2023-08-23 07:36:08 -0500

    update bot

--- tweets.R
+++ tweets.R
@@ -20,27 +20,8 @@ token <- rtweet::create_token(
 
 # compile tweet text ----
 ## get data ----
-### cdc ----
-cdc_champaign_url <- "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=integrated_county_timeseries_fips_17019_external"
-cdc_champaign_data <- rio::import(
-  cdc_champaign_url,
-  format = "json")$integrated_county_timeseries_external_data
-
-cdc_champaign_cases <- cdc_champaign_data |> 
-  select(date, cases_7_day_count_change, deaths_7_day_count_change,
-         percent_positive_7_day) |> 
-  arrange(date) |>
-  mutate(date = ymd(date)) %>%
-  mutate(Date = ymd(date)) |> 
-  mutate(avg_new_cases = cases_7_day_count_change/7) |> 
-  mutate(avg_new_cases = if_else(avg_new_cases == 0,
-                                 NA,
-                                 avg_new_cases)) |> 
-  fill(avg_new_cases, .direction = "down") 
-
-idph_cases_champaign <- cdc_champaign_cases 
-
-## hhs hospitalizations ----
+## Champaign ----
+### hhs hospitalizations ----
 hospitalizations_url <- "https://healthdata.gov/resource/anag-cw7u.json?zip=61801"
 hospitalizations <- rio::import(hospitalizations_url,
                                 format = "json") %>% 
@@ -58,102 +39,53 @@ hospitalizations_by_date <- hospitalizations %>%
   summarise(total = sum(values)) %>%
   group_by(Date) %>%
   summarise(sum_hospitalized = sum(total)) %>%
-  mutate(avg_hospitalized = sum_hospitalized/7) %>%
-  mutate(CountyName = "Champaign")
+  mutate(avg_hospitalized = sum_hospitalized/7) 
 
-## cdc hospitalizations ----
-cdc_champaign_hosp <- cdc_champaign_data %>%
-  select(date, percent_adult_inpatient_beds_used_confirmed_covid,
-         percent_adult_icu_beds_used_confirmed_covid) %>%
-  arrange(date) %>%
-  mutate(date = ymd(date)) %>%
-  mutate(Date = ymd(date)) %>%
-  filter(date > ymd("2020-07-14"))
+iwss_download_url <- "https://iwss.uillinois.edu/wastewater-treatment-plant/download/159/"
+iwss_download <- content(GET(iwss_download_url))
 
-## combine data ----
-idph_cases_vax_hosp <- full_join(idph_cases_champaign, hospitalizations_by_date) %>%
-  full_join(cdc_champaign_hosp) %>%
-  select(Date, avg_new_cases, avg_hospitalized,percent_positive_7_day,
-         percent_adult_inpatient_beds_used_confirmed_covid) %>%
-  arrange(Date)
+iwss <- iwss_download %>%
+  mutate(Date = ymd(sample_collect_date)) %>%
+  filter(method == 1) |> 
+  select(Date, sars_cov_2, method) |> 
+  mutate(sars_cov_2_avg = zoo::rollmean(sars_cov_2,
+                                        k = 4,
+                                        fill = NA,
+                                        align = "right")) 
 
-idph_cases_vax_hosp_longer <- idph_cases_vax_hosp %>%
-  pivot_longer(!Date,
-               values_to = "values",
-               names_to = "names") %>%
-  mutate(names = recode_factor(names, 
-                        "avg_new_cases" = "Average New Cases",
-                        "percent_positive_7_day" = "Test Positivity",
-                        "monthlydead" = "Deaths in the Past Month",
-                        "avg_hospitalized" = "Average Hospitalized",
-                        "percent_adult_inpatient_beds_used_confirmed_covid" = "Percent of Hospital Beds Used by COVID-19 Patients"))  %>%
-  mutate(short_date = paste(month(Date, label = TRUE, abbr = FALSE),
-                            mday(Date))) %>%
-  drop_na()
-
-
-
-
-# idph_cases_vax <- full_join(idph_cases_champaign, idph_vax_champaign) %>%
-#   select(Date, PersonsFullyVaccinated, AdministeredCountRollAvg,
-#          monthlydead, avg_new_cases)
-# 
-# idph_cases_vax_longer <- idph_cases_vax %>%
-#   pivot_longer(!Date,
-#                values_to = "values",
-#                names_to = "names") %>%
-#   mutate(names = recode(names,
-#                         "PersonsFullyVaccinated" = "3. People Fully Vaccinated",
-#                         "avg_new_cases" = "1. Average New Cases",
-#                         "monthlydead" = "2. Deaths in Past Month",
-#                         "AdministeredCountRollAvg" = "4. Average New Vaccine Doses"))  %>%
-#   mutate(short_date = paste(month(Date, label = TRUE, abbr = FALSE),
-#                             mday(Date)))
-
-
-
+combined <- full_join(hospitalizations_by_date,iwss) |> 
+  arrange(Date) |> 
+  fill(avg_hospitalized,.direction = "down") |> 
+  fill(sars_cov_2_avg,.direction = "down") |> 
+  select(Date, avg_hospitalized,sars_cov_2_avg) |> 
+  pivot_longer(!Date) |> 
+  mutate(name = recode_factor(name, 
+                              "avg_hospitalized" = "Avg. Hospitalized",
+                              "sars_cov_2_avg" = "Wastewater Moving Avg."))
 
 ## set variables ----
-champaign_avg_new_cases <- format(round(signif(tail(idph_cases_champaign$avg_new_cases,1),3)),big.mark=",")
-champaign_weekday <- wday(tail(idph_cases_champaign$Date,1), label = TRUE, abbr = FALSE)
-short_today <- wday(today()-days(2), label = TRUE, abbr = FALSE)
-champaign_month_ago_cases <- format(round(signif(tail(lag(idph_cases_champaign$avg_new_cases, 14),1),3)),big.mark=",")
-champaign_case_pct_change <- round(100*(tail(idph_cases_champaign$avg_new_cases,1)-tail(lag(idph_cases_champaign$avg_new_cases, 14),1))/tail(lag(idph_cases_champaign$avg_new_cases, 14),1), digits = 0)
-
-champaign_case_pct_change_text <- 
-  if (champaign_case_pct_change > 0) { 
-    paste("+",champaign_case_pct_change,"%↑", sep = "")
-  } else if (champaign_case_pct_change == 0) {
-    paste("", sep = "")
-  } else { 
-    paste("",champaign_case_pct_change,"%↓", sep = "")
-  }
+latest_data <- tail(combined, n = 1)$Date
+champaign_avg_hospitalized <- format(round(signif(tail(hospitalizations_by_date$avg_hospitalized,1),3)),big.mark=",")
+champaign_month_ago_hospitalized <- 
+  format(round(signif(tail(lag(hospitalizations_by_date$avg_hospitalized,2),1),3)),big.mark=",")
 
 ## make text ----
 
 champaign_county_text <- paste(
-  "As of ",champaign_weekday," (vs. two weeks ago):
-
-",
-"— Average new cases: ",champaign_avg_new_cases," (vs. ",champaign_month_ago_cases,") ",champaign_case_pct_change_text,"
-",
-"
-
-More charts: https://bzigterman.com/projects/covid",
-sep = ""
+  "More charts: https://bzigterman.com/projects/covid",
+  sep = ""
 )
 champaign_county_text
+typeof(champaign_county_text)
 
-# tweet plot ----
-p <- ggplot(idph_cases_vax_hosp_longer,
+# plot ----
+p <- ggplot(combined,
             aes(x = as.Date(Date),
-                y = values,
-                colour = names)) +
+                y = value,
+                colour = name)) +
   geom_line() +
-  facet_wrap(~ names, scales = "free_y") +
-  labs(#title = "Metrics Since Vaccinations Began Dec. 16",
-    caption = paste("Sources: CDC and HHS. Latest data:",
-                    tail(idph_cases_vax_hosp_longer$short_date,1))) +
+  facet_wrap(~ name, scales = "free_y", ncol = 1) +
+  labs(caption = paste("Sources: CDC and IWSS")) +
   xlab(NULL) +
   ylab(NULL) +
   scale_x_date(expand = c(0,0),
@@ -176,17 +108,17 @@ p <- ggplot(idph_cases_vax_hosp_longer,
 p
 # save to a temp file
 file <- tempfile( fileext = ".png")
-ggsave( file, plot = p, device = "png", dpi = 320, width = 8, height = 8*(628/1200))
+ggsave( file, plot = p, device = "png", dpi = 320, width = 4, height = 4)
 
 # post the tweet ----
-if (champaign_avg_new_cases > 0 && 
-    champaign_month_ago_cases >= 0 && 
-    champaign_weekday == short_today
+if (champaign_avg_hospitalized >= 0 && 
+    champaign_month_ago_hospitalized >= 0 && 
+    latest_data >= now(tzone = "America/Chicago") - days(x = 14)
 ) {
   rtweet::post_tweet( 
     status = champaign_county_text,
     media = file,
-    media_alt_text = "Line charts with today's COVID-19 statistics for Champaign County, Illinois",
+    media_alt_text = "Line charts with recent COVID-19 statistics for Champaign County, Illinois",
     token = token
   )
 }
